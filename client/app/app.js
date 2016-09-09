ef1c55f2573696c65517ae8f807eb41998d92f22
Vadym Yatsyuk <vadym.yatsyuk@gmail.com>
2016-09-09 21:39:59 +0200

    added map

--- client/app/app.js
+++ client/app/app.js
@@ -1,10 +1,15 @@
 'use strict';
 
 angular.module('localEyeApp', ['localEyeApp.auth', 'localEyeApp.admin', 'localEyeApp.constants',
-    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'validation.match'
+    'ngCookies', 'ngResource', 'ngSanitize', 'btford.socket-io', 'ui.router', 'validation.match',
+  'uiGmapgoogle-maps'
   ])
-  .config(function($urlRouterProvider, $locationProvider) {
+  .config(function($urlRouterProvider, $locationProvider, uiGmapGoogleMapApiProvider) {
     $urlRouterProvider.otherwise('/');
 
     $locationProvider.html5Mode(true);
+
+    uiGmapGoogleMapApiProvider.configure({
+      china: true
+    });
   });
--- client/app/main/main.controller.js
+++ client/app/main/main.controller.js
@@ -9,36 +9,39 @@
       this.socket = socket;
       this.awesomeThings = [];
 
-      $scope.$on('$destroy', function() {
-        socket.unsyncUpdates('thing');
-      });
+      this.map = { center: { latitude: 45, longitude: -73 }, zoom: 8 };
+
+      // $scope.$on('$destroy', function() {
+      //   socket.unsyncUpdates('thing');
+      // });
     }
 
     $onInit() {
-      this.$http.get('/api/things')
-        .then(response => {
-          this.awesomeThings = response.data;
-          this.socket.syncUpdates('thing', this.awesomeThings);
-        });
+      // this.$http.get('/api/things')
+      //   .then(response => {
+      //     this.awesomeThings = response.data;
+      //     this.socket.syncUpdates('thing', this.awesomeThings);
+      //   });
     }
 
-    addThing() {
-      if (this.newThing) {
-        this.$http.post('/api/things', {
-          name: this.newThing
-        });
-        this.newThing = '';
-      }
-    }
+    // addThing() {
+    //   if (this.newThing) {
+    //     this.$http.post('/api/things', {
+    //       name: this.newThing
+    //     });
+    //     this.newThing = '';
+    //   }
+    // }
 
-    deleteThing(thing) {
-      this.$http.delete('/api/things/' + thing._id);
-    }
+    // deleteThing(thing) {
+    //   this.$http.delete('/api/things/' + thing._id);
+    // }
   }
 
   angular.module('localEyeApp')
     .component('main', {
       templateUrl: 'app/main/main.html',
-      controller: MainController
+      controller: MainController,
+      controllerAs: 'vm'
     });
 })();
